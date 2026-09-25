fbb582695094d68537ef63c439840baa1cb26546
lukeniukrostyslav <lukeniukrostyslav@gmail.com>
2026-09-25 23:23:42 +0200

    chore: reset repository for print product

--- playwright.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require("@playwright/test");
-module.exports = defineConfig({
-  testDir: "./e2e",
-  use: { browserName: "chromium", baseURL: "http://127.0.0.1:4173" },
-  webServer: { command: "python3 -m http.server 4173", port: 4173, reuseExistingServer: true },
-  reporter: [["line"],["html",{open:"never"}]]
-});
